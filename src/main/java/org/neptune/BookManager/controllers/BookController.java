a5e166f4e3a2b3a11057a1862771666ae5d56dec
vertex0427 <shm-sharp@hotmail.com>
2020-09-09 00:48:12 +0800

    更新BookController

--- src/main/java/org/neptune/BookManager/controllers/BookController.java
+++ src/main/java/org/neptune/BookManager/controllers/BookController.java
@@ -17,12 +17,52 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    //主页，显示所有书本
     @RequestMapping(path = {"/index"}, method = {RequestMethod.GET})
     public String bookList(Model model) {
         loadAllBooksView(model);
         return "book/books";
     }
 
+    //书本添加页面
+    @RequestMapping(path = {"/books/add"}, method = {RequestMethod.GET})
+    public String addBook() {
+        return "book/addbook";
+    }
+
+    //新书本输入页面
+    @RequestMapping(path = {"/books/add/do"}, method = {RequestMethod.POST})
+    public String doAddBook(
+            @RequestParam("name") String name,
+            @RequestParam("author") String author,
+            @RequestParam("price") String price
+    ) {
+        Book book = new Book();
+        book.setName(name);
+        book.setAuthor(author);
+        book.setPrice(price);
+        bookService.addBook(book);
+
+        return "redirect:/index";
+    }
+
+    //书本删除页面
+    @RequestMapping(path = {"/books/{bookId:[0-9]+}/delete"}, method = {RequestMethod.GET})
+    public String deleteBook(@PathVariable("bookId") int bookId) {
+
+        bookService.deleteBook(bookId);
+        return "redirect:/index";
+    }
+
+    //书本恢复正常状态页面
+    @RequestMapping(path = {"/books/{bookId:[0-9]+}/recover"}, method = {RequestMethod.GET})
+    public String recoverBook(@PathVariable("bookId") int bookId) {
+        
+        bookService.recoverBook(bookId);
+        return "redirect:/index";
+    }
+
+    //为model加载所有的book
     private void loadAllBooksView(Model model) {
         model.addAttribute("books", bookService.getAllBooks());
     }
